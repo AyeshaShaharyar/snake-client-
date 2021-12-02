fe91d1afd79b6777767b6a50f5525f8e90430878
AyeshaShaharyar <ayesha2598@gmail.com>
2021-12-02 06:32:50 +0000

    snake wrapup

--- client.js
+++ client.js
@@ -1,26 +1,20 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: "50541"
+    host: IP,
+    port: PORT
   });
 
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log("Successfully connected to game server");
-  });
 
-  conn.on('connect', () => {
     conn.write('Name: AS');
-    conn.write('Move: up');
   });
 
-  conn.on("connect", () =>{
-    setTimeout(()=>{
-      conn.write('Move: up')
-    }, 500); 
-  });
+  
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -28,4 +22,4 @@ const connect = function () {
   return conn;
 };
 
-module.exports = {connect};
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,10 +1,9 @@
 
-const {connect} = require("./client");
-const {setupInput} = require("./input");
+const connect = require("./client");
+const setupInput = require("./input");
 
 
-
+const conn = connect();
 
 console.log("Connecting ...");
-connect();
-setupInput();
+setupInput(conn);
